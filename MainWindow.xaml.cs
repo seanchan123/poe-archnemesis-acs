b84a7374ef021e023d5ff880324809217cb5fa97
Sean Chan <seanchan@outlook.sg>
2022-02-09 21:56:39 +0800

    Added global event listener for hotkeys
    To be modified in the future to make hotkeys changeable, have to reference https://docs.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes for the respective values. Able to listen to hotkey even if window not in focus.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,19 +1,10 @@
-﻿using Hardcodet.Wpf.TaskbarNotification;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Windows.Interop;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace poe_archnemesis_acs
 {
@@ -22,6 +13,60 @@ namespace poe_archnemesis_acs
     /// </summary>
     public partial class MainWindow : Window
     {
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        private const int HOTKEY_ID = 9000;
+
+        //Modifiers:
+        private const uint MOD_NONE = 0x0000; //(none)
+        private const uint MOD_ALT = 0x0001; //ALT
+        private const uint MOD_CONTROL = 0x0002; //CTRL
+        private const uint MOD_SHIFT = 0x0004; //SHIFT
+        private const uint MOD_WIN = 0x0008; //WINDOWS
+        //CAPS LOCK:
+        private const uint VK_OEM_3 = 0xC0;
+
+        const int WM_HOTKEY = 0x0312;
+
+        private IntPtr _windowHandle;
+        private HwndSource _source;
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+
+            _windowHandle = new WindowInteropHelper(this).Handle;
+            _source = HwndSource.FromHwnd(_windowHandle);
+            _source.AddHook(HwndHook);
+
+            RegisterHotKey(_windowHandle, HOTKEY_ID, MOD_SHIFT, VK_OEM_3); //SHIFT + ~
+        }
+
+        private IntPtr HwndHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            switch (msg)
+            {
+                case WM_HOTKEY:
+                    switch (wParam.ToInt32())
+                    {
+                        case HOTKEY_ID:
+                            int vkey = (((int)lParam >> 16) & 0xFFFF);
+                            if (vkey == VK_OEM_3)
+                            {
+                                MessageBox.Show("yay");
+                            }
+                            handled = true;
+                            break;
+                    }
+                    break;
+            }
+            return IntPtr.Zero;
+        }
+
         public MainWindow()
         {
             InitializeComponent();
@@ -30,21 +75,21 @@ namespace poe_archnemesis_acs
             WindowStyle = WindowStyle.None;
             ShowInTaskbar = false;
 
-
         }
 
         private void MainWindow_KeyDown(object sender, KeyEventArgs e)
         {
-            if (Keyboard.IsKeyDown(Key.LeftShift) && e.Key == Key.OemTilde)
-            {
-                Console.WriteLine("yes..");
-                this.Show();
-            }
+            //if (Keyboard.IsKeyDown(Key.LeftShift) && e.Key == Key.OemTilde)
+            //{
+            //    Console.WriteLine("yes..");
+            //    this.Show();
+            //}
         }
 
         private void MainWindow_Closing(object sender, CancelEventArgs e)
         {
             MessageBox.Show("Closing called");
         }
+
     }
 }

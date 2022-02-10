6761686cc89e60b49c3c51ba0218e9a6dc408943
Sean Chan <seanchan@outlook.sg>
2022-02-10 23:19:09 +0800

    Added ArchnemesisModModel
    This model helps better facilitate the specificity of each mods.

---
+++ ArchnemesisModModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poe_archnemesis_acs
+{
+    class ArchnemesisModModel
+    {
+        private string name, imgSrc;
+        private string count;
+
+        private List<string> archnemesisMod;
+        private List<List<string>> archnemesisMods;
+
+        public List<List<string>> ArchnemesisMods
+        {
+            get { return archnemesisMods; }
+            set { archnemesisMods = value; }
+        }
+
+        public List<string> ArchnemesisMod
+        {
+            get { return archnemesisMod; }
+            set { archnemesisMod = value; }
+        }
+
+        private string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private string ImageSource
+        {
+            get { return imgSrc; }
+            set { imgSrc = value; }
+        }
+
+        private string Count
+        {
+            get { return count; }
+            set { count = value; }
+        }
+
+
+    }
+}

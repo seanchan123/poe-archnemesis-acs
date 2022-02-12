d7b05ca04dac4e3d4ba8aab477c0e829ac416864
Sean Chan <seanchan@outlook.sg>
2022-02-12 16:57:28 +0800

    Added back all development files

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace poe_archnemesis_acs
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}

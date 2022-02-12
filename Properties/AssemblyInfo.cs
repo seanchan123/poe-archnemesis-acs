01474a6f7f1592d4521edeb58cf145a58b118561
Sean Chan <seanchan@outlook.sg>
2022-02-12 23:30:50 +0800

    Updated AssemblyTitle
    Now reflects as "PoE Archnemesis ACS" instead of "poe-archnemesis-acs" in Task Manager

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("poe-archnemesis-acs")]
+[assembly: AssemblyTitle("PoE Archnemesis ACS")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("poe-archnemesis-acs")]
+[assembly: AssemblyProduct("PoE Archnemesis ACS")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
